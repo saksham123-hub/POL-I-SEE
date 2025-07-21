32e77725c60bc964033eccbfb24f4eee9e1a0673
saksham123-hub <sakshamsinghranawat@gmail.com>
2025-07-21 23:11:20 +0530

    mysql database created

--- IR-Database/ir_policy_db_type.sql
+++ IR-Database/ir_policy_db_type.sql
@@ -1,4 +1,4 @@
-MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
 --
 -- Host: localhost    Database: ir_policy_db
 -- ------------------------------------------------------
